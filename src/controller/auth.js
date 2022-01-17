ee3b25d71686814fadbd0bb70fc8ed0f4a24968e
pranav-mistry <showking00765@gmail.com>
2022-01-17 09:54:31 +0530

    final work with signup and signin

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,4 +1,4 @@
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import User from '../models/userModels'; // import schema
 import { resError, resStatus } from '../utils/func';
 
@@ -9,6 +9,37 @@ export const fetchUserData = async (req, res) => {
 		return resStatus(req, res, { user });
 	} catch (err) {
 		return resError(req, res, { err });
+	}
+};// post request
+export const sendUserData = async (req, res) => {
+	try {
+		const {
+			username, name, email, password, phone,
+		} = req.body; // destructuring of data
+		// check try and catch method for new configuration
+		let hash;
+		try {
+			hash = await bcrypt.hash(password, 10);
+		} catch (err) {
+			return resError(req, res, { err: 'There is a hashing in method' });
+		} // hashing try catch
+
+		const user = new User({
+			username,
+			name,
+			email,
+			password: hash,
+			phone,
+		});
+
+		try {
+			await user.save(); // it will save the data
+			return resStatus(req, res, { user });
+		} catch (err) {
+			return resError(req, res, { err: 'Data is not inserted' });
+		} // user try catch
+	} catch (err) {
+		return resError(req, res, { err: 'Proccess not done ...' });
 	}
 };
 
@@ -25,19 +56,31 @@ export const getIdData = async (req, res) => {
 // post request
 export const sendUserData = async (req, res) => {
 	try {
-		const { username,	name, email, password, phone } = req.body; // destructuring of data
+		const {
+			username, name, email, password, phone,
+		} = req.body; // destructuring of data
+		// check try and catch method for new configuration
+		let hash;
+		try {
+			hash = await bcrypt.hash(password, 10);
+		} catch (err) {
+			return resError(req, res, { err: 'There is a hashing in method' });
+		} // hashing try catch
 
-		// const hash = await bcrypt.hash(password, 10);
 		const user = new User({
 			username,
 			name,
 			email,
-			password,
+			password: hash,
 			phone,
 		});
-		await user.save(); // it will save the data
-		// console.log(user);
-		return resStatus(req, res, { user });
+
+		try {
+			await user.save(); // it will save the data
+			return resStatus(req, res, { user });
+		} catch (err) {
+			return resError(req, res, { err: 'Data is not inserted' });
+		} // user try catch
 	} catch (err) {
 		return resError(req, res, { err: 'Proccess not done ...' });
 	}
